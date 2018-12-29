9469a95947e2d77c702145bc0a47764ce9969bea
Math <mateusz.hinc95@gmail.com>
2018-12-29 12:31:56 +0100

    plot labeling, ignore lowercase naming errors

--- plotting.py
+++ plotting.py
@@ -1,8 +1,18 @@
 import matplotlib.pyplot as plt
+
 from model import SimpleCalculationStructure
 
 
-def plot(*args: SimpleCalculationStructure):
+class PlotConfig:
+
+    def __init__(self, xLabel="Czas [s]", yLabel="poziom cieczy [l]"):
+        self.xLabel = xLabel
+        self.yLabel = yLabel
+
+
+def plot(*args: SimpleCalculationStructure, config: PlotConfig = PlotConfig()):
     for cs in args:
         plt.plot(cs.time, cs.values)
+    plt.xlabel(config.xLabel)
+    plt.ylabel(config.yLabel)
     plt.show()
